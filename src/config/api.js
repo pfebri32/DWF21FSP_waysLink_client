477dd4ce49b68d08c52f5a2d2d1918fe9261889f
pfebri32 <personal.febri32@gmail.com>
2021-04-19 05:43:50 +0700

    Update for deploy

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  baseURL: 'http://localhost:5001/api/v2',
+  baseURL: 'https://wayslink-api.herokuapp.com/api/v2',
 });
 
 export const setAuthToken = (token) => {
